20932b5046a54d1255aa28a06c4bef223d89fadc
Heejin <lalabori@naver.com>
2024-08-14 17:48:36 +0900

    second commit

--- src/component/WeatherBox.js
+++ src/component/WeatherBox.js
@@ -2,6 +2,11 @@ import React from "react";
 
 const WeatherBox = ({ weather }) => {
   console.log("weather", weather)
+  const temperatureC = 
+  weather && weather.main
+      ? (weather.main.temp).toFixed(2)
+      : "";
+
   const temperatureF =
     weather && weather.main
       ? ((weather.main.temp  * 9/5) + 32).toFixed(2)
@@ -11,7 +16,7 @@ const WeatherBox = ({ weather }) => {
   return (
     <div className="weather-box">
       <div> {weather?.name} </div>
-      <h2>  {weather.main.temp} °C / {temperatureF} °F </h2>
+      <h2>  {temperatureC} °C / {temperatureF} °F </h2>
       <h3> {weather?.weather[0].description} </h3>
     </div>
   );
